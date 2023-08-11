d2c41a0a84d5015cc12445ae6a0287cc58ac87c0
Aditya Dubey <adityadubey7895@gmail.com>
2023-08-11 13:18:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import CreateArea from './components/CreateArea';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import React from 'react';
+import { useState } from 'react';
+import Note from './components/Note';
+// import notes from './notes';
+
+// function createNotes(noteItem) {
+//   return (
+//     <Note
+//       key={noteItem.key}
+//       title={noteItem.title}
+//       content={noteItem.content}
+//     />
+//   );
+// }
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Header />
+        <CreateArea onAdd={addNote} />
+        {notes.map((noteItem, index) => {
+          return (
+            <Note
+              key={index}
+              id={index}
+              title={noteItem.title}
+              content={noteItem.content}
+              onDelete={deleteNote}
+            />
+          );
+        })}
+        <Footer />
+        {/* <Note title="this is the note title" content="this is the note content" /> */}
+        {/* {notes.map(createNotes)} */}
+      </div>
+    </>
   );
 }
-
 export default App;
